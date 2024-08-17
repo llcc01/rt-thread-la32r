cc0d534291ce3c1c990a56c2c119cfff53b1c129
llcc01 <2417224420@qq.com>
2024-08-17 23:03:21 +0800

    1c102: update mem addr

--- libcpu/loongarch/la132/start.h
+++ libcpu/loongarch/la132/start.h
@@ -2,10 +2,10 @@
 #define _START_H_
 
 /******************  CLK CONFIG  ******************/
-#define RAM1_BASE           (0x80000000)
+#define RAM1_BASE           (0x90000000)
 // #define RAM2_BASE           (0x80002800)
 // #define RAM2_BASE           (0x80004000)
-#define RAM2_BASE           (0x80100000)
+#define RAM2_BASE           (0x90100000)
 // #define SP_BASE             (0x80002000-132)
 // #define REGS_MEM          (0x80004000) //first use in REGS_MEM-4
 
